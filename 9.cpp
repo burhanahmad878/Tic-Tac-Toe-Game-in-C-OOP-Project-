e5e61aaf9663008fbaa71c5b5d08caab6fd268ce
Burhan Ahmad <burhanahmadcase@gmail.com>
2026-04-10 15:45:08 +0500

    Refactor TicTacToe to include player names

--- 9.cpp
+++ 9.cpp
@@ -1,94 +1,140 @@
 #include <iostream>
+#include <string>
+#include <limits>
 using namespace std;
 
-class TicTacToe {
+class TicTacToe
+{
 private:
+    string player1, player2;
     char board[3][3];
     char turn;
     bool draw;
 
 public:
-    TicTacToe() {
-        // Initialize board with numbers 1-9
+    TicTacToe()
+    {
+        cout << "Enter Player 1 Name (X): ";
+        getline(cin, player1);
+
+        cout << "Enter Player 2 Name (O): ";
+        getline(cin, player2);
+
         char count = '1';
         for (int i = 0; i < 3; i++)
             for (int j = 0; j < 3; j++)
                 board[i][j] = count++;
+
         turn = 'X';
         draw = false;
     }
 
-    void displayBoard() {
-        system("cls"); // use "clear" if on Linux
+    void displayBoard()
+    {
+        system("cls");
         cout << "\n";
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 3; i++)
+        {
             cout << "     |     |     \n";
-            for (int j = 0; j < 3; j++) {
+            for (int j = 0; j < 3; j++)
+            {
                 cout << "  " << board[i][j] << "  ";
-                if (j < 2) cout << "|";
+                if (j < 2)
+                    cout << "|";
             }
             cout << "\n";
-            if (i < 2) cout << "_____|_____|_____\n";
-            else cout << "     |     |     \n";
+            if (i < 2)
+                cout << "_____|_____|_____\n";
+            else
+                cout << "     |     |     \n";
         }
     }
 
-    void playerTurn() {
+    void playerTurn()
+    {
         int choice;
-        cout << "\nPlayer " << turn << "'s turn. Enter your choice: ";
-        cin >> choice;
 
-        int row = (choice - 1) / 3;
-        int col = (choice - 1) % 3;
+        while (true)
+        {
+            if (turn == 'X')
+                cout << "\n" << player1 << " (X)'s turn. Enter your choice: ";
+            else
+                cout << "\n" << player2 << " (O)'s turn. Enter your choice: ";
 
-        if (choice < 1 || choice > 9 || board[row][col] == 'X' || board[row][col] == 'O') {
-            cout << "Invalid move! Try again.\n";
-            playerTurn(); // can also use a loop instead of recursion
-        } else {
-            board[row][col] = turn;
-            turn = (turn == 'X') ? 'O' : 'X';
+            cin >> choice;
+
+            if (cin.fail())
+            {
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                cout << "Invalid input! Enter a number from 1 to 9.\n";
+                continue;
+            }
+
+            int row = (choice - 1) / 3;
+            int col = (choice - 1) % 3;
+
+            if (choice >= 1 && choice <= 9 && board[row][col] != 'X' && board[row][col] != 'O')
+            {
+                board[row][col] = turn;
+                turn = (turn == 'X') ? 'O' : 'X';
+                break;
+            }
+            else
+            {
+                cout << "Invalid move! Try again.\n";
+            }
         }
     }
 
-    bool gameOver() {
-        // Check rows and columns
-        for (int i = 0; i < 3; i++) {
+    bool gameOver()
+    {
+        for (int i = 0; i < 3; i++)
+        {
             if (board[i][0] == board[i][1] && board[i][1] == board[i][2])
                 return false;
             if (board[0][i] == board[1][i] && board[1][i] == board[2][i])
                 return false;
         }
-        // Check diagonals
+
         if (board[0][0] == board[1][1] && board[1][1] == board[2][2])
             return false;
         if (board[0][2] == board[1][1] && board[1][1] == board[2][0])
             return false;
 
-        // Check for draw
         draw = true;
         for (int i = 0; i < 3; i++)
             for (int j = 0; j < 3; j++)
                 if (board[i][j] != 'X' && board[i][j] != 'O')
                     draw = false;
 
-        if (draw) return false; // game over with draw
+        if (draw)
+            return false;
 
-        return true; // continue playing
+        return true;
     }
 
-    void announceResult() {
+    void announceResult()
+    {
         displayBoard();
-        if (draw) {
+        if (draw)
+        {
             cout << "Game Draw!\n";
-        } else {
-            // Previous player made the winning move
+        }
+        else
+        {
             char winner = (turn == 'X') ? 'O' : 'X';
-            cout << "Player " << winner << " wins! Congratulations!\n";
+            if (winner == 'X')
+                cout << player1 << " wins! Congratulations!\n";
+            else
+                cout << player2 << " wins! Congratulations!\n";
         }
     }
 
-    void play() {
-        while (gameOver()) {
+    void play()
+    {
+        while (gameOver())
+        {
             displayBoard();
             playerTurn();
         }
@@ -96,9 +142,23 @@ public:
     }
 };
 
-int main() {
-    TicTacToe game;
+int main()
+{
+    char choice;
+
     cout << "================== Welcome to Tic Tac Toe Game! ==================\n";
-    game.play();
+
+    do
+    {
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        TicTacToe game;
+        game.play();
+
+        cout << "\nDo you want to play again? (y/n): ";
+        cin >> choice;
+
+    } while (choice == 'y' || choice == 'Y');
+
+    cout << "Thanks for playing!\n";
     return 0;
 }
